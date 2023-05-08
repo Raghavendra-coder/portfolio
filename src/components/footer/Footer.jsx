8beb61d6a5fbb49e51c0f2815b7b5ab312848c3d
raghavendra-coder <rkeshari50@gmail.com>
2023-05-09 01:05:04 +0530

    removed logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import {BsLinkedin, BsInstagram, BsGithub} from 'react-icons/bs';
 const Footer = () => {
   return (
     <footer>
-      <a href='#' className="footer__logo">Raghavendra</a>
+      <a href='#' className="footer__logo"></a>
       <ul className='permalinks'>
         <li><a href='#'>Top</a></li>
         <li><a href='#about'>About</a></li>
